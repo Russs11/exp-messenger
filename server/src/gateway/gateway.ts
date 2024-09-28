9fd8048d3aeea891927d1a41e037f2b093b9c3ed
exp <arzuakov@yahoo.com>
2024-09-28 13:01:38 +0300

    h28.09.2024

--- server/src/gateway/gateway.ts
+++ server/src/gateway/gateway.ts
@@ -1,13 +1,31 @@
+import { OnModuleInit } from '@nestjs/common';
 import {
+  ConnectedSocket,
   MessageBody,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
+import { Server,Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: { origin: '*' }, transports: ['websocket'] })
-export class MyGateway {
+@WebSocketGateway()
+export class MyGateway implements OnModuleInit {
+  @WebSocketServer()
+  server: Server;
+
+  onModuleInit() {
+    this.server.on('connection', (socket) => {
+      console.log('connected ' + socket.id);
+    });
+  }
+
   @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
-    console.log('body: ', body);
+  onNewMessage(@MessageBody() body: any, @ConnectedSocket() client: Socket) {
+    console.log(`body`, body);
+    console.log(`from`, client.id);
+    this.server.emit('onMessage', {
+      msg: 'New Message',
+      content: body,
+    });
   }
 }
