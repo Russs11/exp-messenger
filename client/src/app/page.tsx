98e7e8e7f9a838c34b5965843e401e7cac9ea508
arzuakov@yahoo.com <arzuakov@yahoo.com>
2024-11-06 17:50:42 +0300

    w06.11.2024

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
+import { WebsocketProvider, socket } from './context/WebsocetContext'
 
 export default function Home() {
-	return (
+  return (
+    <WebsocketProvider value={socket}>
     <div className='grid grid-rows-[50px_1fr_50px] items-center justify-items-center min-h-screen gap-1 p-2'>
       <div className='grid bg-slate-500 h-full w-2/4 items-center justify-items-center'>
         <Link href='/login'>login</Link>
@@ -14,5 +16,7 @@ export default function Home() {
         3
       </div>
     </div>
+
+    </WebsocketProvider>
   )
 }
