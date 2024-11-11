9bd1afb440499094522640b20bc716380d5efe3f
arzuakov@yahoo.com <arzuakov@yahoo.com>
2024-11-11 16:48:22 +0300

    w11.11.2024

--- client/src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <div className='grid grid-rows-1 items-center justify-items-center min-h-screen gap-1 p-2'>
-      <div className='grid items-center justify-items-center h-52 w-80 text-black font-bold bg-green-300'>
-        default login
-      </div>
-    </div>
-  )
-}
