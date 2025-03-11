3980f65f5518a515084d9d19029ef9e9f735c5d7
arzuakov@yahoo.com <arzuakov@yahoo.com>
2025-03-11 17:51:25 +0300

    11.03.2025

---
+++ client/src/app/components/cardContainerSeparator.tsx
@@ -0,0 +1,6 @@
+import { ReactElement } from 'react'
+import styles from './styles/cardContainerSeparator.module.scss'
+
+export default function CardContainerSeparator(): ReactElement {
+  return <div className={styles.cardContainerSeparator}></div>
+}
