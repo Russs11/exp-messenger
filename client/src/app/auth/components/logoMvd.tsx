d117c66d459d4eda5ceb1d5562b98c094b4a3696
Russs11 <russs.khalilov@gmail.com>
2025-04-21 22:58:23 +0300

    h21.04.2025_auth form

--- client/src/app/auth/components/logoMvd.tsx
+++ client/src/app/auth/components/logoMvd.tsx
@@ -1,7 +1,9 @@
-import { ReactElement } from 'react'
 import Image from 'next/image'
+import { ReactElement } from 'react'
 import logoMvd from '../../../img/LogoMVD.svg'
 
 export default function LogoMvd(): ReactElement {
-  return <Image src={logoMvd} alt={'logo MVD'} width={300} />
+  return (
+    <Image src={logoMvd} alt={'logo MVD'} width={250} priority />
+  )
 }
